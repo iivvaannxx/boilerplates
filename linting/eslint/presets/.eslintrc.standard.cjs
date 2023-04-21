16e0a1fb60dc09baf647342ab4a4a534fd2d8f93
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-04-21 17:26:59 +0200

    StandardJS rule adjustment.

--- linting/eslint/presets/.eslintrc.standard.cjs
+++ linting/eslint/presets/.eslintrc.standard.cjs
@@ -1,13 +1,46 @@
-// Dependencies: [ 'standard' ]
+// Dependencies: [ 'eslint', 'eslint-config-standard' ]
+
+/** @brief A couple of opinionated adjustments to the StandardJS rules. */
+const preferredRules = {
+
+  'padded-blocks': 'off',
+  'padding-line-between-statements': [
+
+    'error',
+
+    { blankLine: 'always', prev: 'directive', next: '*' },
+    { blankLine: 'any', prev: 'directive', next: 'directive' },
+
+    { blankLine: 'always', prev: 'import', next: '*' },
+    { blankLine: 'any', prev: 'import', next: 'import' },
+    { blankLine: 'always', prev: 'export', next: '*' },
+    { blankLine: 'any', prev: 'export', next: 'export' },
+
+    { blankLine: 'any', prev: '*', next: 'return' },
+    { blankLine: 'always', prev: ['case', 'default'], next: '*' },
+
+    { blankLine: 'always', prev: '*', next: 'block-like' },
+    { blankLine: 'always', prev: '*', next: 'block' }
+  ]
+}
 
 /** @brief ESLint configuration which uses the StandardJS linting rules. */
 const config = {
 
+  env: {
+
+    browser: true,
+    node: true
+  },
+
   extends: [
 
     // Use the StandardJS rules.
-    './node_modules/standard/eslintrc.json'
-  ]
+    'eslint-config-standard'
+  ],
+
+  root: true,
+  rules: preferredRules
 }
 
 
